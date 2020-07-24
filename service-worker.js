70321b90dc3376c9b922281153c9c94396fbee52
anakeeb <anakeeb@purdue.edu>
2020-07-24 14:59:39 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/financial-forecast-io/precache-manifest.b50f80abf157595b608fec8397e4b4d1.js"
+  "/financial-forecast-io/precache-manifest.f617ced780ce04887bb8338826f81ba4.js"
 );
 
 self.addEventListener('message', (event) => {
